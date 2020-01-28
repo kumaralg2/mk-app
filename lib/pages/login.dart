3c297fae86897f0d34ac34483b771b359db57f92
kiran <kumar.alg2@gmail.com>
2020-01-28 13:30:18 +0530

    test

---
+++ lib/pages/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class Login extends StatelessWidget {
+  static const
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text('Login Screen'),
+      ),
+
+    );
+  }
+}
